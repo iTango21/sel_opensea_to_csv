801d38dc5730f1eee126ae8d6ba63e5ff53cd634
iTango21 <itango21@gmail.com>
2022-05-12 15:46:18 +0300

    download source page...

--- main.py
+++ main.py
@@ -27,3 +27,28 @@ import csv
 import pandas as pd
 # df = pd.read_csv('data.csv', sep=';', header=0)
 
+url = 'https://opensea.io/rankings?sortBy=one_day_volume'
+
+
+print('start...')
+
+options = webdriver.FirefoxOptions()
+options.set_preference("general.useragent.override", f"{ua}")
+
+s = Service('geckodriver.exe')
+
+driver = webdriver.Firefox(service=s, options=options)
+
+driver.implicitly_wait(1.5)
+hhh = driver.get(url)
+# driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+
+time.sleep(3)
+html = driver.page_source
+
+# with requests.Session() as session:
+#     response = session.get(url=url, headers=headers)
+#
+# запись СПАРСЕНОЙ инфы в ХТМЛ-файл
+with open('index.html', 'w', encoding='utf-8') as file:
+    file.write(html)
