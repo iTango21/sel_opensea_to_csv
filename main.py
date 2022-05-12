c2d462c494aea07cf0c85dcdef07284e1a98e143
iTango21 <itango21@gmail.com>
2022-05-12 16:57:47 +0300

    obtaining the necessary data...

--- main.py
+++ main.py
@@ -46,40 +46,56 @@ print('start...')
 # # driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 #
 # time.sleep(3)
-# html = driver.page_source
+# source_html = driver.page_source
 #
-#
+# time.sleep(2)
+# driver.close()
+# driver.quit()
 # #
-# # with requests.Session() as session:
-# #     response = session.get(url=url, headers=headers)
-# #
-# # запись СПАРСЕНОЙ инфы в ХТМЛ-файл
-# with open('index.html', 'w', encoding='utf-8') as file:
-#     file.write(html)
-
-# 2
+# # #
+# # # with requests.Session() as session:
+# # #     response = session.get(url=url, headers=headers)
+# # #
+# # # # запись СПАРСЕНОЙ инфы в ХТМЛ-файл
+# # with open('index.html', 'w', encoding='utf-8') as file:
+# #     file.write(source_html)
 #
-#soup = BeautifulSoup(html, 'lxml')
+# # # 2
+# # #
+# # #soup = BeautifulSoup(html, 'lxml')
+# #
+# # with open("index.html", "r", encoding='utf-8') as f:
+# #     source_html = f.read()
+# #
+# soup = BeautifulSoup(source_html, 'lxml')
+#
+#
+# """
+# в коде страницы есть скрипт:
+# <script id="__NEXT_DATA__" ... </scrypt>
+#
+# в этом скрипте между тегами:
+# "json": и "data: лежит готовый JSON с данными!!!
+#
+# ...достаём его:
+# """
+# script_all = soup.find('script', id='__NEXT_DATA__')
+# script_ = str(re.findall('\"json\"\:\{\"data\"(.*?)\"data\"\:', str(script_all))).\
+#                                                                 replace(",']", "").\
+#                                                                 replace("['", "").\
+#                                                                 replace('\\\\"', "'")
+# json_all = '{"data"'+script_
+# #scr1 = '{"data"'+ (str(re.findall('\"json\"\:\{\"data\"(.*?)\"data\"\:', str(script))).replace(",']", "").replace("['", ""))
+#
+# with open('_my_json.json.', 'w', encoding='utf-8') as file:
+#     #json.dump(json_all, file, indent=4, ensure_ascii=False)
+#     file.write(json_all)
 
-with open("index.html", "r", encoding='utf-8') as f:
-    source_html = f.read()
-
-soup = BeautifulSoup(source_html, 'lxml')
-
-"""
-в коде страницы есть скрипт:
-<script id="__NEXT_DATA__" ... </scrypt>
-
-в этом скрипте между тегами:
-"json": и "data: лежит готовый JSON с данными!!!
-
-...достаём его:
-"""
-script_all = soup.find('script', id='__NEXT_DATA__')
-script_ = str(re.findall('\"json\"\:\{\"data\"(.*?)\"data\"\:', str(script_all))).replace(",']", "").replace("['", "")
-json_all = '{"data"'+script_
-#scr1 = '{"data"'+ (str(re.findall('\"json\"\:\{\"data\"(.*?)\"data\"\:', str(script))).replace(",']", "").replace("['", ""))
-
-with open('_my_json.json.', 'w', encoding='utf-8') as file:
-    json.dump(json_all, file, indent=4, ensure_ascii=False)
-    #file.write(json_all)
+# 3
+#
+with open("_my_json.json", "r") as read_file:
+    data_ = json.load(read_file)
+#dict_obj = json.loads('_my_json.json')
+for aaa in range(0, 2):
+    print(data_['data']['rankings']['edges'][aaa]['node']['name'])
+    print(data_['data']['rankings']['edges'][aaa]['node']['statsV2']['oneDayVolume']['unit'])
